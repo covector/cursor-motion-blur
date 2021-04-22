0115c0c3c2d23a8d2dd0e5a93d127cd7fe37b290
covector <60296643+covector@users.noreply.github.com>
2021-04-23 00:17:18 +0800

    fixed color not set after disable and enable extension

--- background.js
+++ background.js
@@ -6,8 +6,9 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.action.onClicked.addListener(() => {
     chrome.storage.sync.get("active", ({ active }) => {
-        chrome.storage.sync.set({active: !active});
+        chrome.storage.sync.set({active: active == null ? true : !active});
         chrome.action.setBadgeText({text: !active ? "ON" : "OFF"});
+        chrome.action.setBadgeBackgroundColor({color: "#42495c"});
     });
 });
 
